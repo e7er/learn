dfeaf3f747047c0ac8dd8639d47502e90b194add
a <75105275@qq.com>
2022-05-04 10:48:10 +0800

    feat: init

---
+++ app/src/main/java/com/e7er/learn/MainActivity.java
@@ -0,0 +1,85 @@
+package com.e7er.learn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener, View.OnLongClickListener {
+
+    TextView show;
+    View back, next;
+    static List<String> arrShow = new ArrayList<>();
+    static int position = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        show = findViewById(R.id.main_show);
+        back = findViewById(R.id.main_back);
+        next = findViewById(R.id.main_next);
+
+        back.setOnClickListener(this);
+        next.setOnClickListener(this);
+        back.setOnLongClickListener(this);
+        next.setOnLongClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        arrShow = Arrays.asList(this.getResources().getStringArray(R.array.chinese));
+        Collections.shuffle(arrShow);
+        show(++position);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_back:
+                show(--position);
+                break;
+            case R.id.main_next:
+                show(++position);
+                break;
+        }
+    }
+
+    private void show(int pointer) {
+        int p = (pointer + arrShow.size()) % arrShow.size();
+        show.setText(arrShow.get(p));
+    }
+
+    /**
+     * 长按切换显示内容
+     *
+     * @param view
+     * @return
+     */
+    @Override
+    public boolean onLongClick(View view) {
+        switchArray();
+        return false;
+    }
+
+    /**
+     * 切换数组
+     */
+    private void switchArray() {
+        String[] chinese = this.getResources().getStringArray(R.array.chinese);
+        String[] phonetic = this.getResources().getStringArray(R.array.phonetic);
+        if (arrShow.size() == chinese.length) {
+            arrShow = Arrays.asList(phonetic);
+        } else {
+            arrShow = Arrays.asList(chinese);
+        }
+        Collections.shuffle(arrShow);
+    }
+}
